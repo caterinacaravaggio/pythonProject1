5c2c6095c4841108243074b9f16ef3c8f798216a
caterinacaravaggio <caterina.caravaggio@studio.unibo.it>
2020-09-14 09:39:51 +0200

    port changing

--- userservice.py
+++ userservice.py
@@ -13,8 +13,6 @@ with open("app.conf.yaml", 'r') as stream:
     except yaml.YAMLError as err:
         print(err)
 
-
-
 logging.basicConfig(filename=config['logger']['filename'], level=config['logger']['level'])
 
 logging.info("Program started")
@@ -28,25 +26,30 @@ logging.info("Connected to DB")
 with client:
     logging.info("Current UserDetails \n" + '\n'.join(str(elem) for elem in list(db.userDetails.find())))
 
-#json validation
+
+# json validation
 def validate_json(jsonData):
     result = "ok"
-    try: jsonData['userName']
+    try:
+        jsonData['userName']
     except (KeyError, ValueError) as err:
         logging.error(err.args[0] + " in json file")
         result = err.args[0] + " in json file"
         pass
-    try: jsonData['name']
+    try:
+        jsonData['name']
     except (KeyError, ValueError) as err:
         logging.error(err.args[0] + " in json file")
         result = err.args[0] + " in json file"
         pass
-    try: jsonData['surname']
+    try:
+        jsonData['surname']
     except (KeyError, ValueError) as err:
         logging.error(err.args[0] + " in json file")
         result = err.args[0] + " in json file"
         pass
-    try: age = int(jsonData['age'])
+    try:
+        age = int(jsonData['age'])
     except (KeyError, ValueError) as err:
         logging.error(err.args[0] + " in json file")
         result = err.args[0] + " in json file"
@@ -55,49 +58,51 @@ def validate_json(jsonData):
 
     if (age <= 0):
         logging.error("incorrect age")
-        result =  "incorrect age"
+        result = "incorrect age"
         pass
     logging.info("Row inserted successfully")
 
     return result
 
 
-#create core functions
+# create core functions
 def insertUser(request):
     logging.info("Request for insert user")
     code = 1
-    #insert in db
+    # insert in db
     with client:
-        #validate request
+        # validate request
         ok = validate_json(request)
         if (ok != "ok"):
             code = -2
-            logging.error("Not valid insert: " + ok )
+            logging.error("Not valid insert: " + ok)
         logging.info("Starting insert")
-        try: db.userDetails.insert(request)
+        try:
+            db.userDetails.insert(request)
         except Exception as err:
             logging.error("Failed insert" + err.args[0])
             code = -2
-            ok =  "Failed insert" + err.args[0]
+            ok = "Failed insert" + err.args[0]
             pass
-        #create json string to return
+        # create json string to return
         if code < 0:
             message = '{ "code" : ' + str(code) + ', "defalutMessage" : ' + ok + '}'
         else:
             message = '{ "code" : ' + str(code) + ', "defalutMessage" : "user has been inserted", ' \
                       + str(request) + ' }'
     return json.dumps(message)
-#look at api with json
 
+
+# look at api with json
 
 
 def getUserDetails(userName):
     logging.info("Request for find user")
     code = 1
-    #retrive value from db assuming only one value
+    # retrive value from db assuming only one value
     with client:
-        findUser = list(db.userDetails.find({'userName': userName}, {'_id' : 0}))
-        #print(findUser)
+        findUser = list(db.userDetails.find({'userName': userName}, {'_id': 0}))
+        # print(findUser)
         if not findUser:
             logging.error("Failed find: no matching")
             code = -1
@@ -107,11 +112,11 @@ def getUserDetails(userName):
         if code < 0:
             message = '{ "code" : ' + str(code) + ', "defalutMessage" : ' + ok + '}'
         else:
-            message = '{ "code" : ' + str(code) + ', "defalutMessage" : "user has been found", ' + '\n'.join(str(elem) for elem in findUser) + '}'
+            message = '{ "code" : ' + str(code) + ', "defalutMessage" : "user has been found", ' + '\n'.join(
+                str(elem) for elem in findUser) + '}'
         jsonResult = json.dumps(message)
 
     return jsonResult
-
 
 
 @app.route(config['routes']['insert']['url'], methods=['GET', 'POST'])
@@ -123,6 +128,7 @@ def insert():
         logging.error("GET request ")
         return 'Error: no %s method' % escape(request.methods)
 
+
 @app.route(config['routes']['get']['url'], methods=['GET', 'POST'])
 def find():
     logging.info("Find Request")
@@ -131,3 +137,7 @@ def find():
     else:
         logging.error("POST request ")
         return 'Error %s not method' % escape(request.methods)
+
+
+if __name__ == '__main__':
+    app.run(port=9000)
